d334efa3094b7f767452b08e56503b588bf3bc5c
Guilherme Monte <gmonte@inf.ufsm.br>
2020-08-09 20:45:16 -0300

    build: v3

--- dist/utils/common-values.js
+++ dist/utils/common-values.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 exports.selectionMaxWidth = exports.actionsColumnWidth = exports.rowActions = exports.baseIconSize = exports.elementSize = void 0;
 
